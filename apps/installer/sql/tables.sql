c2aae10d097b255af5513bc4a5077f3459886481
Jan Stanik <jan.stanik@creat.io>
2011-06-02 20:37:30 +0100

    Empty file for SQL statement creating N7 specific tables during the installation.

---
+++ apps/installer/sql/tables.sql
@@ -0,0 +1,9 @@
+
+--- @file tables.sql
+--- @author giorno
+--- @package N7
+--- @license Apache License, Version 2.0, see LICENSE file
+---
+--- Script installing database tables specific for N7 solution. These extend
+--- base tables installed by framework script.
+
