8b0ffc3ed65ee2b9580dd2ff70c29b345befa902
Jan Stanik <jan.stanik@creat.io>
2011-07-02 08:29:31 +0100

    Making LogMeOut widget independent from any widgets pool layout.

--- apps/signed/lib/_wwg.LogMeOut.php
+++ apps/signed/lib/_wwg.LogMeOut.php
@@ -22,7 +22,10 @@ class LogMeOut extends Wwg
 		$this->id = static::ID;
 		$this->template = dirname(__FILE__) . '/../templ/_wwg.LogMeOut.html';
 		_app_registry::getInstance( )->requireJs( 'inc/signed/_wwg.LogMeOut.js', $this->id );
+		$spacer = new Spacer( );
+		_wwg_registry::getInstance( )->register( _wwg_registry::POOL_MENU, $spacer->getId( ), $spacer );
 		_wwg_registry::getInstance( )->register( _wwg_registry::POOL_MENU, $this->id, $this );
+		_wwg_registry::getInstance( )->setLayout( _wwg_registry::POOL_MENU, Array( n7_ui::getInstance( )->getMenu( )->getId( ), $spacer->getId( ), LogMeOut::ID ) );
 		_smarty_wrapper::getInstance()->getEngine( )->assign( 'WWG_LOGMEOUT_USERNAME', _session_wrapper::getInstance()->getNick( ) );
 	}
 }
