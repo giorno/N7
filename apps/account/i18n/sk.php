c6f0ec8dde51ae1c37f720aea33cff6c54b9bc02
Jan Stanik <jan.stanik@creat.io>
2011-08-31 20:03:12 +0100

    Housekeeping in My Account app localization.

--- apps/account/i18n/sk.php
+++ apps/account/i18n/sk.php
@@ -1,27 +1,30 @@
 <?PHP
-/*
+
+/**
  * @file sk.php
- *
- * My Account tab internationalization file for Slovak language.
- *
  * @author giorno
+ * @package N7
+ * @subpackage Account
+ * @license Apache License, Version 2.0, see LICENSE file
+ * 
+ * Slovak language localization for Account app.
  */
 
-$__msgAcc['tabName']				= 'Môj účet';
+$__msgAcc['tabName']					= 'Môj účet';
 
 /**
  * Settings editor form.
  */
-$__msgAcc['sem']['btSave']			= 'Uložiť nastavenia';
-$__msgAcc['sem']['btReset']			= 'Obnoviť';
-$__msgAcc['sem']['collGlobal']		= 'Globálne nastavenia';
-$__msgAcc['sem']['aLanguage']		= 'Jazyk';
-$__msgAcc['sem']['aTimezone']		= 'Časová zóna';
-$__msgAcc['sem']['aListLen']		= 'Veľkosť stránky';
-$__msgAcc['sem']['saving']			= 'Ukladám...';
-$__msgAcc['sem']['saved']			= 'Uložené';
-$__msgAcc['sem']['e_unknown']		= 'Chyba: Neznáma chyba! Kontaktujte správcov.';
-$__msgAcc['sem']['e_data']			= 'Chyba: chybná hodnota!';
+$__msgAcc['sem']['btSave']				= 'Uložiť nastavenia';
+$__msgAcc['sem']['btReset']				= 'Obnoviť';
+$__msgAcc['sem']['collGlobal']			= 'Globálne nastavenia';
+$__msgAcc['sem']['aLanguage']			= 'Jazyk';
+$__msgAcc['sem']['aTimezone']			= 'Časová zóna';
+$__msgAcc['sem']['aListLen']			= 'Veľkosť stránky';
+$__msgAcc['sem']['saving']				= 'Ukladám...';
+$__msgAcc['sem']['saved']				= 'Uložené';
+$__msgAcc['sem']['e_unknown']			= 'Chyba: Neznáma chyba! Kontaktujte správcov.';
+$__msgAcc['sem']['e_data']				= 'Chyba: chybná hodnota!';
 
 /**
  * Change password form.
@@ -37,86 +40,8 @@ $__msgAcc['chpass']['ind']['e_unknown']	= $__msgAcc['sem']['e_unknown'];
 $__msgAcc['chpass']['ind']['doing']		= 'Vykonávam..';
 $__msgAcc['chpass']['ind']['done']		= 'Heslo zmenené';
 
-$__msgAcc['capWishlist']			= 'Prajem si...';
-
-$__msgAcc['foldSettings']			= 'Nastavenia';
-$__msgAcc['capSettings']			= 'Centrálna správa nastavení vášho účtu';
-
-$__msgAcc['foldChPass']					= 'Zmeniť heslo';
-$__msgAcc['capChPass']					= 'Pre vyššiu bezpečnosť meňte svoje heslo každé 3 mesiace';
-
-$__msgAcc['capPassword']			= 'Zmeniť heslo';
-$__msgAcc['capInvitation']			= 'Pozvať priateľa';
-$__msgAcc['capWriteUs']				= 'Napísať adminom';
-
-$__msgAcc['back2Wishlist']			= 'Späť na Prehľad';
-
-$__msgAcc['wishSettings']			= 'Zmeniť nastavenia';
-$__msgAcc['wishPassword']			= $__msgAcc['capPassword'];
-$__msgAcc['wishInvitation']			= $__msgAcc['capInvitation'];
-$__msgAcc['wishWriteUs']			= $__msgAcc['capWriteUs'];
-
-$__msgAcc['blahSettings']			= 'Zmeňte svoju e-mailovú adresu, jazyk, časovú zónu a ďalšie nastavenia, aby ste sa cítili pohodlne s našou jedinečnou službou. Myslíme si, že skutočné šťastie pochádza z pravej slobody.';
-$__msgAcc['blahPassword']			= 'Neberte bezpečnostné riziko na ľahkú váhu a meňte svoje heslo aspoň každé tri mesiace. Používajte znaky ako čísla, zátvorky, atď., aby ste mali skutočne silné heslo.';
-$__msgAcc['blahInvitation']			= 'Pošlite pozvánku každému koho poznáte a o kom si myslíte, že by mu bola naša služba užitočná. Aj keď pre nás nie je dôležité množstvo užívateľov, vždy je skvelé mať okolo seba komunitu ľudí plných nápadov a skúseností. Ako adresa odosielateľa bude uvedená adresa z nastavení vášho účtu.';
-$__msgAcc['blahWriteUs']			= 'Objavili ste v našej službe chybu? Alebo máte nápady na vylepšenie? Prípadne len potrebujete napísať adminom? Tento formulár je ten správny nástroj pre Vašu aktuálnu potrebu.';
-
-$__msgAcc['fieldEmail']				= 'E-mailová adresa';
-/*$__msgAcc['fieldLang']				= 'Jazyk';
-$__msgAcc['fieldTimezone']			= 'Časová zóna';
-$__msgAcc['fieldPagesize']			= 'Veľkosť stránky';*/
-$__msgAcc['fieldLifegoals']			= 'Životné ciele';
-/*$__msgAcc['optionNoLifegoals']		= 'Vypnúť životné ciele';
-$__msgAcc['optionAllBoxes']			= 'Zo všetkých schránok';
-$__msgAcc['optionOnlySd']			= 'Len zo schránky Niekedy/Možno';
-$__msgAcc['optionAllExceptAr']		= 'Zo všetkých schránok okrem Archívu';*/
-/*$__msgAcc['fieldBpAlg']				= 'Farba počtu vecí';
-$__msgAcc['optionSimpleMath']		= 'Jednoduchý priemer priorít';
-$__msgAcc['optionHofstadter']		= 'Hofstadterov algoritmus';
-$__msgAcc['optionStatic']			= 'Vypnúť farby';*/
-/*$__msgAcc['fieldTimePresets']		= 'Predvoľby času';
-$__msgAcc['optionPredefined']		= 'Systémové';
-$__msgAcc['option1week']			= 'Za posledný týždeň';
-$__msgAcc['option30days']			= 'Za posledných 30 dní';
-$__msgAcc['option60days']			= 'Za posledných 60 dní';
-$__msgAcc['option90days']			= 'Za posledných 90 dní';
-$__msgAcc['optionAllTime']			= 'Od prvého prihlásenia';
-$__msgAcc['option3presets']			= '3 predvoľby';
-$__msgAcc['option4presets']			= '4 predvoľby';
-$__msgAcc['option5presets']			= '5 predvolieb';
-$__msgAcc['option6presets']			= '6 predvolieb';
-$__msgAcc['option7presets']			= '7 predvolieb';
-$__msgAcc['option0presets']			= 'Vypnúť predvoľby času';*/
-$__msgAcc['fieldOldPass']			= 'Vaše súčasné heslo';
-$__msgAcc['fieldNewPass']			= 'Nové heslo';
-$__msgAcc['fieldConfirm']			= 'Potvrdenie hesla';
-$__msgAcc['fieldInvitationTo']		= 'Adresa priateľa';
-$__msgAcc['fieldSubject']			= 'Predmet (nepovinné)';
-$__msgAcc['fieldMessage']			= 'Správa';
-
-$__msgAcc['descEmail']				= 'Toto je dôležitá informácia. Vaše heslo je posielané na túto adresu v prípade, že ho zabudnete.';
-$__msgAcc['descInvitationTo']		= 'Vložte adresu priateľa, ktorého chcete pozvať, a kliknite na tlačidlo Pozvať.';
-$__msgAcc['descWriteUs']			= 'Napíšte nám všetko čo máte na srdci a kliknite na tlačidlo Odoslať.';
-
-$__msgAcc['btSave']					= 'Uložiť nastavenia';
-$__msgAcc['btReset']				= 'Reset';
-$__msgAcc['btChange']				= $__msgAcc['capPassword'];
-$__msgAcc['btInvite']				= 'Pozvať';
-$__msgAcc['btSend']					= 'Odoslať';
-
-//$__msgAcc['formErrors']				= $__msgGtd['regErrorErrors'];
-//$__msgAcc['formExplain']			= $__msgGtd['regErrorExplanation'];
-
-$__msgAcc['statusSettSaved']		= 'Vaše nové nastavenia boli uložené.';
-//$__msgAcc['statusSettErrEmail']		= $__msgGtd['regErrorInvAddr'];
-
-$__msgAcc['statusPassErrOld']		= 'Vložili ste nesprávne súčasné heslo!';
-$__msgAcc['statusPassErrNew']		= 'Nové heslo má nepovolený formát!';
-$__msgAcc['statusPassErrConfirm']	= 'Potvrdenie hesla nesúhlasí!';
-$__msgAcc['statusInvWrongAddress']	= 'Adresa má nepovolený formát!';
-$__msgAcc['statusPassSaved']		= 'Heslo bolo zmenené. Použijete ho pri ďalšom prihlásení do služby.';
-$__msgAcc['statusInvited']			= 'Pozvánka bola odoslaná.';
-$__msgAcc['statusWrittenUs']		= 'Vaša správa bola odoslaná. Ďakujeme.';
-$__msgAcc['statusWriteUsEmpty']		= 'Telo správy nesmie byť prázdne!';
+$__msgAcc['foldSettings']				= 'Nastavenia';
+$__msgAcc['capSettings']				= 'Centrálna správa nastavení vášho účtu';
+$__msgAcc['capChPass']					= 'Zmeniť heslo';
 
 ?>
