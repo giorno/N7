8af9d3c57578a0bc606630a85f441504958dc143
Jan Stanik <jan.stanik@creat.io>
2012-06-14 11:30:28 +0100

    Implementing current development version upgrade path.

---
+++ apps/installer/sql/delta/0.1.1-betato0.1.2-dev.sql
@@ -0,0 +1,14 @@
+
+--- @file 0.1.1-betato0.1.2-dev.sql
+--- @author giorno
+--- @package N7
+--- @license Apache License, Version 2.0, see LICENSE file
+---
+--- Installation script for uprade of MySQL tables. Used in upgrade script
+--- for upgrades from version 0.1.1-beta to 0.1.2-dev.
+
+---
+--- Required for PDO implementation of io\creat\chassis\session\settings class.
+---
+ALTER TABLE `core_settings` ADD UNIQUE `core_settings_index` ( `scope`, `id`, `ns`, `key` );
+
